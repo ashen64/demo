20fb5420660efda1270e0c33794d6a8824321354
ctx <2020302727@qq.com>
2024-07-28 19:17:28 +0800

    第3次提交项目

--- src/main/java/com/ctx/test/mapper/MyMapper.java
+++ src/main/java/com/ctx/test/mapper/MyMapper.java
@@ -3,6 +3,10 @@ package com.ctx.test.mapper;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 反反复复反反复复333
+ */
+
 @org.apache.ibatis.annotations.Mapper
 public interface MyMapper {
     List<Map<String,Object>> getList();
